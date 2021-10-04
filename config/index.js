b6f885d669a26fbbfb01ace95664ef73aad6fb84
lakal0809 <intelliguy@outlook.com>
2021-10-04 22:02:36 +0800

    add config file

---
+++ config/index.js
@@ -0,0 +1,12 @@
+export const CLUSTERS = {
+  MAINNET: 'mainnet-beta',
+  TESTNET: 'testnet',
+  DEVNET: 'devnet',
+};
+
+export const COMMAND_PREFIX = '.';
+
+export const DISCORD_TOKEN='ODkyMTI0Mzk1MDg5Mjk3NDg4.YVIVlg.ZOycFiSPymyjdGEr_-09oEXtzL0'
+
+export const gSAIL_TOKEN_ADDRESS='so2UmtgXmc1mMk7GKNtPfCNeWuDjEMUa5JsEGYg8x7t'
+export const SAIL_TOKEN_ADDRESS='4WkdBnsUt3zyWkhVbXgY9aQeR64ri42ioYTuaZp8WATn'
