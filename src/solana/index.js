a41fed73de95df949f158b2c78e2a3fb90d6fe20
jrg3k5 <jrg3k5@gmail.com>
2022-02-08 16:57:44 -0500

    Added transaccions confirmation and proper messages

--- src/solana/index.js
+++ src/solana/index.js
@@ -1,39 +1,39 @@
 import web3 from '@solana/web3.js'
 import splToken from '@solana/spl-token'
 import SessionStorageService from '../wallet/SessionStorageService.js';
-import { 
-  CLUSTERS,
-  gSAIL_TOKEN_ADDRESS, 
-  SAIL_TOKEN_ADDRESS 
+import {
+  ACTIVE_CLUSTER,
+  gSAIL_TOKEN_ADDRESS,
+  SAIL_TOKEN_ADDRESS
 } from '../../config/index.js'
 import DB from '../publicKeyStorage/index.js'
 
-const createWallet = async (id) => { 
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
+const createWallet = async (id) => {
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
   var wallet = web3.Keypair.generate();
-  
+
   await Promise.all([
     SessionStorageService.setKeyPair(id, wallet.secretKey, wallet.publicKey.toString()),
-    SessionStorageService.setCluster(id, CLUSTERS.DEVNET),
+    SessionStorageService.setCluster(id, ACTIVE_CLUSTER),
   ]);
 
   try {
     await DB.saveUser({
       discordId: id,
       publicKey: wallet.publicKey.toString()
-    });  
+    });
   } catch (error) {
     console.log("Cannot save the user to database");
   }
-  
+
   return {
     privateKey: wallet.secretKey,
     publicKey: wallet.publicKey.toString(),
   };
 };
 
-const importWallet = async (id, keyArr) => { 
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
+const importWallet = async (id, keyArr) => {
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
   let wallet;
   try {
     wallet = web3.Keypair.fromSecretKey(new Uint8Array(keyArr));
@@ -42,21 +42,21 @@ const importWallet = async (id, keyArr) => {
       status: false,
     };
   }
-  
+
   await Promise.all([
     SessionStorageService.setKeyPair(id, wallet.secretKey, wallet.publicKey.toString()),
-    SessionStorageService.setCluster(id, CLUSTERS.DEVNET),
+    SessionStorageService.setCluster(id, ACTIVE_CLUSTER),
   ]);
 
   try {
     await DB.saveUser({
       discordId: id,
       publicKey: wallet.publicKey.toString()
-    });  
+    });
   } catch (error) {
     console.log("Cannot save the user to database");
   }
-  
+
   return {
     status: true,
     privateKey: wallet.secretKey,
@@ -65,14 +65,14 @@ const importWallet = async (id, keyArr) => {
 };
 
 const getSolBalance = async (publicKey) => {
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
 
   try {
     let amount = await connection.getBalance(new web3.PublicKey(publicKey)) / web3.LAMPORTS_PER_SOL;
 
     return {
       status: true,
-      amount: amount, 
+      amount: amount,
     };
   } catch (error) {
     return {
@@ -83,16 +83,16 @@ const getSolBalance = async (publicKey) => {
 };
 
 const getGSAILBalance = async (privateKey) => {
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
   var wallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(privateKey)));
 
   let token = await new splToken.Token(
-    connection, 
-    new web3.PublicKey(gSAIL_TOKEN_ADDRESS), 
+    connection,
+    new web3.PublicKey(gSAIL_TOKEN_ADDRESS),
     splToken.TOKEN_PROGRAM_ID,
     wallet
   );
-  
+
   let account;
 
   try {
@@ -104,7 +104,7 @@ const getGSAILBalance = async (privateKey) => {
     return {
       isExistToken: false,
       amount: 0
-    }; 
+    };
   }
 
   return {
@@ -114,12 +114,12 @@ const getGSAILBalance = async (privateKey) => {
 }
 
 const getSAILBalance = async (privateKey) => {
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
   var wallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(privateKey)));
 
   let token = await new splToken.Token(
-    connection, 
-    new web3.PublicKey(SAIL_TOKEN_ADDRESS), 
+    connection,
+    new web3.PublicKey(SAIL_TOKEN_ADDRESS),
     splToken.TOKEN_PROGRAM_ID,
     wallet
   );
@@ -145,15 +145,17 @@ const getSAILBalance = async (privateKey) => {
 }
 
 const transferSOL = async (fromPrivateKey, toPubKey, sol, desc) => {
-  var fromWallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(fromPrivateKey)));
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
-  
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
+  let signature,
+      fromWallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(fromPrivateKey)));
+
   try {
+
     // Add transfer instruction to transaction
     let transaction = new web3.Transaction().add(
       web3.SystemProgram.transfer({
         fromPubkey: fromWallet.publicKey,
-        toPubkey: toPubKey,
+        toPubkey: new web3.PublicKey(toPubKey),
         lamports: sol * web3.LAMPORTS_PER_SOL,
       }),
     ).add(new web3.TransactionInstruction({
@@ -163,27 +165,28 @@ const transferSOL = async (fromPrivateKey, toPubKey, sol, desc) => {
     }));
 
     // Sign transaction, broadcast, and confirm
-    let signature = await web3.sendAndConfirmTransaction(
+    signature = await web3.sendAndConfirmTransaction(
       connection,
       transaction,
       [fromWallet],
-    ); 
+    );
 
     console.log(signature);
   } catch (error) {
-    return false;
+    console.log(`error: ${error},\nstack: ${error.stack}`);
+    return {success: false, error};
   }
 
-  return true;
+    return {success: true, signature};
 };
 
 const transferSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
-
-  var fromWallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(fromPrivateKey)));
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
+  let signature,
+      fromWallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(fromPrivateKey)));
 
   let token = await new splToken.Token(
-    connection, 
+    connection,
     new web3.PublicKey(SAIL_TOKEN_ADDRESS),
     splToken.TOKEN_PROGRAM_ID,
     fromWallet
@@ -196,7 +199,8 @@ const transferSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
       fromWallet.publicKey
     )
   } catch (error) {
-    return false;
+    console.log(`error: ${error},\nstack: ${error.stack}`);
+    return {success: false, error};
   }
 
   let toTokenAccount;
@@ -205,7 +209,8 @@ const transferSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
       new web3.PublicKey(toPubKey),
     );
   } catch (error) {
-    return false;
+    console.log(`error: ${error},\nstack: ${error.stack}`);
+    return {success: false, error};
   }
 
   try {
@@ -223,8 +228,8 @@ const transferSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
       programId: new web3.PublicKey("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"),
       data: Buffer.from(desc),
     }));
-        
-    let signature = await web3.sendAndConfirmTransaction(
+
+    signature = await web3.sendAndConfirmTransaction(
       connection,
       transaction,
       [fromWallet],
@@ -233,19 +238,20 @@ const transferSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
 
     console.log(signature);
   } catch (error) {
-    return false;
+    console.log(`error: ${error},\nstack: ${error.stack}`);
+    return {success: false, error};
   }
 
-  return true;
+  return {success: true, signature};
 }
 
 const transferGSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
-  const connection = new web3.Connection(web3.clusterApiUrl(CLUSTERS.DEVNET), 'confirmed');
-
-  var fromWallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(fromPrivateKey)));
+  const connection = new web3.Connection(web3.clusterApiUrl(ACTIVE_CLUSTER), 'confirmed');
+  let signature,
+      fromWallet = web3.Keypair.fromSecretKey(new Uint8Array(Object.values(fromPrivateKey)));
 
   let token = await new splToken.Token(
-    connection, 
+    connection,
     new web3.PublicKey(gSAIL_TOKEN_ADDRESS),
     splToken.TOKEN_PROGRAM_ID,
     fromWallet
@@ -258,7 +264,8 @@ const transferGSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
       fromWallet.publicKey
     )
   } catch (error) {
-    return false;
+    console.log(`error: ${error},\nstack: ${error.stack}`);
+    return {success: false, error};
   }
 
   let toTokenAccount;
@@ -267,7 +274,8 @@ const transferGSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
       new web3.PublicKey(toPubKey),
     );
   } catch (error) {
-    return false;
+    console.log(`error: ${error},\nstack: ${error.stack}`);
+    return {success: false, error};
   }
 
   try {
@@ -285,8 +293,8 @@ const transferGSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
       programId: new web3.PublicKey("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr"),
       data: Buffer.from(desc),
     }));
-        
-    let signature = await web3.sendAndConfirmTransaction(
+
+    signature = await web3.sendAndConfirmTransaction(
       connection,
       transaction,
       [fromWallet],
@@ -294,10 +302,11 @@ const transferGSAIL = async (fromPrivateKey, toPubKey, amount, desc) => {
     );
     console.log(signature);
   } catch (error) {
-    return false;
+    console.log(`error: ${error},\nstack: ${error.stack}`);
+    return {success: false, error};
   }
 
-  return true;
+    return {success: true, signature};
 }
 
 export default {
@@ -308,5 +317,5 @@ export default {
   getSAILBalance,
   transferSOL,
   transferSAIL,
-  transferGSAIL
+  transferGSAIL,
 };
